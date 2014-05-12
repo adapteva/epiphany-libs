8d9f61042386a7297201bcc24543512d748b0325
Ben Chaco <bchaco@x3-c.com>
2014-05-12 18:25:54 -0400

    Removed eclipse managed make files (makefile, *.mk)

--- src/e-hal/Release/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := ../../e-loader/Release/src/e-loader.o ../../e-loader/Release/src/e-process-SREC.o
-
-LIBS :=
-
