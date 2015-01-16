d11a627af372c7cb061a7093b2dbaa68ae39f4b7
YOSHIFUJI Naoki <lwisteria.ao@gmail.com>
2015-01-16 17:31:03 +0900

    e-loader: Fix some compiler warnings
    Use "'const char*' executable" for e_load
    Patch by Yoshifuji Naoki <lwisteria.ao@gmail.com>
    
    * src/e-loader/src/e-loader.c: Make 'executable' argument const
    to suppress warnings.
    * src/e-loader/src/e-loader.h: Likewise.
    * src/e-loader/src/e-process-SREC.c: Likewise.
    
    [[Fixed commit message and added Changelog entry ]]
    Signed-off-by: Ola Jeppsson <ola@adapteva.com>
    
    fixes #28

--- src/e-loader/src/e-loader.h
+++ src/e-loader/src/e-loader.h
@@ -41,8 +41,8 @@ typedef enum {
 	L_D4 = 40,
 } e_loader_diag_t;
 
-int e_load(char *executable, e_epiphany_t *dev, unsigned row, unsigned col, e_bool_t start);
-int e_load_group(char *executable, e_epiphany_t *dev, unsigned row, unsigned col, unsigned rows, unsigned cols, e_bool_t start);
+int e_load(const char *executable, e_epiphany_t *dev, unsigned row, unsigned col, e_bool_t start);
+int e_load_group(const char *executable, e_epiphany_t *dev, unsigned row, unsigned col, unsigned rows, unsigned cols, e_bool_t start);
 
 e_loader_diag_t e_set_loader_verbosity(e_loader_diag_t verbose);
 
