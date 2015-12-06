609dfb57f3b786bb4dddcf0828aa022064121ada
Ola Jeppsson <ola@adapteva.com>
2015-12-06 17:27:52 +0100

    e-hal: Remove SREC support
    This was long overdue, good riddance. This will break some of the
    epiphany-examples temporarily.
    
    /
    	* e-hal/Makemodule.am (libe_loader_la_SOURCES): Remove
    	e-process-SREC.c
    	* e-hal/src/e-loader.c (int e_load_group): Deprecate SREC support.
    	(ee_process_ELF): Move function here from e-process-SREC.c
    	* e-hal/src/e-process-SREC.c: Remove.
    
    Signed-off-by: Ola Jeppsson <ola@adapteva.com>

--- e-hal/src/e-loader.c
+++ e-hal/src/e-loader.c
@@ -34,11 +34,12 @@
 #include "e-loader.h"
 #include "esim-target.h"
 
+typedef unsigned int uint;
+
 #define diag(vN)   if (e_load_verbose >= vN)
 
-e_return_stat_t ee_process_ELF(const char *executable, e_epiphany_t *pEpiphany, e_mem_t *pEMEM, int row, int col);
-e_return_stat_t ee_process_SREC(const char *executable, e_epiphany_t *pEpiphany, e_mem_t *pEMEM, int row, int col);
-int ee_set_core_config(e_epiphany_t *pEpiphany, e_mem_t *pEMEM, int row, int col);
+static e_return_stat_t ee_process_ELF(const char *executable, e_epiphany_t *pEpiphany, e_mem_t *pEMEM, int row, int col);
+static int ee_set_core_config(e_epiphany_t *pEpiphany, e_mem_t *pEMEM, int row, int col);
 
 e_loader_diag_t e_load_verbose = 0;
 
@@ -113,7 +114,10 @@ int e_load_group(const char *executable, e_epiphany_t *dev, unsigned row, unsign
 			}
 			else if (!strncmp(hdr, srecHdr, 2))
 			{
-				diag(L_D1) { fprintf(diag_fd, "e_load_group(): loading SREC file %s ...\n", executable); }
+				diag(L_D1) { fprintf(diag_fd, "e_load_group(): ERROR: SREC support removed\n"); }
+				warnx("ERROR: SREC file support is deprecated. Use elf format.\n");
+				e_free(pemem);
+				return E_ERR;
 			}
 			else
 			{
@@ -130,7 +134,7 @@ int e_load_group(const char *executable, e_epiphany_t *dev, unsigned row, unsign
 					if (iself)
 						retval = ee_process_ELF(executable, dev, pemem, irow, icol);
 					else
-						retval = ee_process_SREC(executable, dev, pemem, irow, icol);
+						retval = E_ERR;
 
 					if (retval == E_ERR)
 					{
@@ -211,3 +215,97 @@ e_loader_diag_t e_set_loader_verbosity(e_loader_diag_t verbose)
 	return old_load_verbose;
 }
 
+static e_return_stat_t ee_process_ELF(const char *executable, e_epiphany_t *pEpiphany, e_mem_t *pEMEM, int row, int col)
+{
+	FILE       *elfStream;
+	Elf32_Ehdr hdr;
+	Elf32_Phdr *phdr;
+	e_bool_t   islocal, isonchip;
+	int        ihdr;
+	void       *pto;
+	unsigned   globrow, globcol;
+	unsigned   CoreID;
+	int        status = E_OK;
+	void      *buf = NULL;
+	uint32_t   addr;
+
+
+	islocal   = E_FALSE;
+	isonchip  = E_FALSE;
+
+	elfStream = fopen(executable, "rb");
+
+	fread(&hdr, sizeof(hdr), 1, elfStream);
+	phdr = alloca(sizeof(*phdr) * hdr.e_phnum);
+	fseek(elfStream, hdr.e_phoff, SEEK_SET);
+	fread(phdr, sizeof(*phdr), hdr.e_phnum, elfStream);
+
+	for (ihdr=0; ihdr<hdr.e_phnum; ihdr++)
+	{
+		if (phdr[ihdr].p_vaddr & 0xfff00000)
+		{
+			// This is a global address. Check if address is on an eCore.
+			islocal  = E_FALSE;
+			isonchip = e_is_addr_on_chip((void *) phdr[ihdr].p_vaddr);
+		} else {
+			// This is a local address.
+			islocal  = E_TRUE;
+			isonchip = E_TRUE;
+		}
+
+		diag(L_D3) { fprintf(diag_fd, "ee_process_ELF(): copying the data (%d bytes)", phdr[ihdr].p_filesz); }
+
+		if (esim_target_p()) {
+			addr = phdr[ihdr].p_vaddr;
+			if (islocal) {
+				addr |= (pEpiphany->core[row][col].id << 20);
+			}
+			buf = realloc(buf, phdr[ihdr].p_filesz);
+			fseek(elfStream, phdr[ihdr].p_offset, SEEK_SET);
+			fread(buf, phdr[ihdr].p_filesz, sizeof(char), elfStream);
+			if (ES_OK != es_ops.mem_store(pEMEM->esim, addr, phdr[ihdr].p_filesz, (uint8_t *) buf)) {
+				fprintf(diag_fd, "ee_process_ELF(): Error: ESIM error writing to 0x%x", addr);
+				return E_ERR;
+			}
+		} else {
+			if (islocal)
+			{
+				// If this is a local address
+				diag(L_D3) { fprintf(diag_fd, " to core (%d,%d)\n", row, col); }
+				pto = pEpiphany->core[row][col].mems.base + phdr[ihdr].p_vaddr; // TODO: should this be p_paddr instead of p_vaddr?
+			}
+			else if (isonchip)
+			{
+				// If global address, check if address is of an eCore.
+				CoreID = phdr[ihdr].p_vaddr >> 20;
+				ee_get_coords_from_id(pEpiphany, CoreID, &globrow, &globcol);
+				diag(L_D3) { fprintf(diag_fd, " to core (%d,%d)\n", globrow, globcol); }
+				pto = pEpiphany->core[globrow][globcol].mems.base + (phdr[ihdr].p_vaddr & ~(0xfff00000)); // TODO: should this be p_paddr instead of p_vaddr?
+			}
+			else
+			{
+				// If it is not on an eCore, it is on external memory.
+				diag(L_D3) { fprintf(diag_fd, " to external memory.\n"); }
+				pto = (void *) phdr[ihdr].p_vaddr;
+				if ((phdr[ihdr].p_vaddr >= pEMEM->ephy_base) && (phdr[ihdr].p_vaddr < (pEMEM->ephy_base + pEMEM->emap_size)))
+				{
+					diag(L_D3) { fprintf(diag_fd, "ee_process_ELF(): converting virtual (0x%08x) ", (uint) phdr[ihdr].p_vaddr); }
+					pto = pto - (pEMEM->ephy_base - pEMEM->phy_base);
+					diag(L_D3) { fprintf(diag_fd, "to physical (0x%08x)...\n", (uint) phdr[ihdr].p_vaddr); }
+				}
+				diag(L_D3) { fprintf(diag_fd, "ee_process_ELF(): converting physical (0x%08x) ", (uint) phdr[ihdr].p_vaddr); }
+				pto = pto - (uint) pEMEM->phy_base + (uint) pEMEM->base;
+				diag(L_D3) { fprintf(diag_fd, "to offset (0x%08x)...\n", (uint) pto); }
+			}
+			fseek(elfStream, phdr[ihdr].p_offset, SEEK_SET);
+			fread(pto, phdr[ihdr].p_filesz, sizeof(char), elfStream);
+		}
+	}
+
+	if (esim_target_p())
+		free(buf);
+
+	fclose(elfStream);
+
+	return status;
+}
